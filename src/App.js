41be2521d3aab80aa12e70553101cbe7336c3dd1
thomher <50306944+thomher@users.noreply.github.com>
2019-05-07 12:37:32 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div>
+          <center><img src="https://static1.squarespace.com/static/5a5977a7f43b55eff1588911/t/5c3cc96e6d2a73d481066085/1547487618372/Workshop+DevOps+Desde+Cero.png?format=1500w" /></center>
+        </div>
       </div>
     );
   }
